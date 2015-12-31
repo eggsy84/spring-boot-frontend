b91eeced533662de9efcad4092c789fb7973a90c
eggsy84 <eggsy@eggsylife.co.uk>
2015-12-31 15:27:16 +0000

    Include intro hello page

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String greeting() {
+
+        return "hello";
+    }
+
+}
